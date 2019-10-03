b545faaf8a972db4b3738c44434739a9281cd71c
leaf-hsiao <leaf_hsiao@outlook.com>
2019-10-03 14:34:24 -0400

    fix test file

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Test cases for user stories 
+"""
+
 import unittest
 from main import parse_file
 
@@ -7,5 +14,5 @@ class UserStoryTest(unittest.TestCase):
     def test_test(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-
-unittest.main(exit=False, verbosity=2)
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
