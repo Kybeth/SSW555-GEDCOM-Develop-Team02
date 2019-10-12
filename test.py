73d9246d7032297d680e2c9f6f4eac3ef30651c7
Tanvi-412 <49460152+Tanvi-412@users.noreply.github.com>
2019-10-12 11:43:18 -0400

    .

--- test.py
+++ test.py
@@ -26,6 +26,17 @@ class UserStoryTest(unittest.TestCase):
         expect = [['ERROR US07', '@I22@'], ['ERROR US07', '@I24@'], ['ERROR US07', '@I25@']]
         result = UserStoryTest.test_file.US07()
         self.assertEqual(expect, result)
+        
+    def test_us05(self):
+        expect = [['ANOMALY US05', '@I1@'],['ANOMALY US05', '@I7@'],['ANOMALY US05', '@I8@'],['ANOMALY US05', '@I9@'],['ANOMALY US05', '@I11@'],['ANOMALY US05', '@I12@'],['ANOMALY US05', '@I14@'],['ANOMALY US05', '@I15@'],['ANOMALY US05', '@I17@'],['ANOMALY US05', '@I18@'],['ANOMALY US05', '@I19@'],['ANOMALY US05', '@I21@'],['ANOMALY US05', '@I24@']]
+        result = UserStoryTest.test_file.US05()
+        self.assertEqual(expect, result)
+    
+    def test_us06(self):
+        expect = [['ANOMALY US06', '@I14@'],['ANOMALY US06', '@I15@']]
+        result = UserStoryTest.test_file.US06()
+        self.assertEqual(expect, result)
+
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
