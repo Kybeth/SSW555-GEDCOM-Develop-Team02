66d0a6c938ad3819486e4e1b9f17b0adfde53e02
Abezzam10 <abezzam@stevens.edu>
2019-10-18 12:03:06 -0400

    Validating imports

--- test2.py
+++ test2.py
@@ -5,25 +5,25 @@ from main2 import Repo
 class UserStoryTest(unittest.TestCase):
     
     """US03"""
-    def test_validate_death_after_birth(self):
+    def test_US03(self):
         """US03 - Birth before Death of Individual"""
         path = 'Das.ged'
         repo = Repo()
         repo.read_file(path)
-        self.assertEqual(repo.validate_death_after_birth(), True)
-        self.assertNotEqual(repo.validate_death_after_birth(), False)
-        self.assertTrue(repo.validate_death_after_birth())
+        self.assertEqual(repo.US03(), True)
+        self.assertNotEqual(repo.US03(), False)
+        self.assertTrue(repo.US03())
 
     """US04"""
 
-    def test_validate_family_marriage_before_divorce(self):
+    def test_US04(self):
         """US04	Marriage before divorce"""
         path = 'Das.ged'
         repo = Repo()
         repo.read_file(path)
-        self.assertEqual(repo.validate_family_marriage_before_divorce(), True)
-        self.assertNotEqual(repo.validate_family_marriage_before_divorce(), False)
-        self.assertTrue(repo.validate_family_marriage_before_divorce())
+        self.assertEqual(repo.US04(), True)
+        self.assertNotEqual(repo.US04(), False)
+        self.assertTrue(repo.US04())
 
     """US13"""
 
@@ -32,20 +32,20 @@ class UserStoryTest(unittest.TestCase):
         path = 'Das.ged'
         repo = Repo()
         repo.read_file(path)
-        self.assertEqual(repo.validate_sibiling_spacing(), True)
-        self.assertNotEqual(repo.validate_sibiling_spacing(), False)
-        self.assertTrue(repo.validate_sibiling_spacing())
+        self.assertEqual(repo.US13(), True)
+        self.assertNotEqual(repo.US13(), False)
+        self.assertTrue(repo.US13())
 
     """US14"""
 
-    def test_validate_multiple_births(self):
+    def test_US14(self):
         """US14 - Multiple births <= 5"""
         path = 'Das.ged'
         repo = Repo()
         repo.read_file(path)
-        self.assertEqual(repo.validate_multiple_births(), True)
-        self.assertNotEqual(repo.validate_multiple_births(), False)
-        self.assertTrue(repo.validate_multiple_births())
+        self.assertEqual(repo.US14(), True)
+        self.assertNotEqual(repo.US14(), False)
+        self.assertTrue(repo.US14())
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
